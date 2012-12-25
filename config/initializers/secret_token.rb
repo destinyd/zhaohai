babfe8dacb591f097cf07369377d17e0e742a023
destinyd <destinyd_war@163.com>
2012-12-25 14:36:56 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zhaohai::Application.config.secret_token = 'ecbe2515d39bd3aee05a6d7071aa10718b7dd3f9a5e0c346ad3bb7ec186a6aaa860e4edd1a69991bb7e83a97b4e03c516b6d9c931cf2db99b253bd905d530105'
