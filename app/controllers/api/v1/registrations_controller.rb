af0c90f32657d5498706b3ac4de21c31e0cd392f
destinyd <destinyd_war@163.com>
2013-03-11 17:21:20 +0800

    reg and test

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,13 +1,24 @@
 module Api::V1
   class RegistrationsController < ApiController
     def create
-      user = User.new(params[:user])
-      if user.save
-        render :json=> user.as_json(:auth_token=>user.authentication_token, :email=>user.email), :status=>201
-        return
+      @user = User.new(params[:user])
+      if @user.save
+        @access_token = Doorkeeper::AccessToken.create!({
+          :application_id     => Doorkeeper::Application.first.id,
+          :resource_owner_id  => @user.id,
+          :expires_in         => nil,
+          :use_refresh_token  => true
+        })
+        render json: {
+          access_token:@access_token.token,
+          token_type: 'bearer',
+          expires_in: nil,
+          refresh_token: @access_token.refresh_token,
+          scope: ''
+        }, :status=>201
       else
         warden.custom_failure!
-        render :json=> user.errors, :status=>422
+        render :json=> @user.errors, :status=>422
       end
     end
 
@@ -27,12 +38,12 @@ module Api::V1
         refresh_token: @access_token.refresh_token,
         scope: '',
         email: @user.email
-      }
+      }, :status=>201
       #if @user = User.create_phone_test
-        #render :json=> @user#, :status=>201
+      #render :json=> @user#, :status=>201
       #else
-        ##warden.custom_failure!
-        #render :json=> @user.errors, :status=>422
+      ##warden.custom_failure!
+      #render :json=> @user.errors, :status=>422
       #end
     end
 
